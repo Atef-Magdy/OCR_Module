1d209e715ec84ae265ca480a38f37f57767bb95e
Atef Magdy <atefmagdy12@gmail.com>
2025-01-21 15:34:03 +0200

    Update find packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="OCR_Module",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=["OCR", "OCR.*"]),
     install_requires=[],
     author="*",
     author_email="*",
@@ -12,7 +12,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Atef-Magdy/OCR_Module",
     classifiers=[
-        "Programming Language :: Python :: 3.6^",
+        "Programming Language :: Python :: 3.13^",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.13',
 )
