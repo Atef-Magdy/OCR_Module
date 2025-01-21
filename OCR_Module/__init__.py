d4199837b98c9c474d1baa6bb7013d6240845d3c
Atef Magdy <atefmagdy12@gmail.com>
2025-01-21 17:23:28 +0200

    Changing full structure

--- OCR_Module/__init__.py
+++
@@ -1,1 +0,0 @@
-from OCR.hello import hello
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="OCR_Module",
+    name="Pipeline",
     version="0.1",
-    packages=find_packages(include=["OCR", "OCR.*"]),
+    packages=find_packages(),
     install_requires=[],
     author="*",
     author_email="*",
